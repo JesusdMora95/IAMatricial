98c5cd8539cac3d963822c2bfe960cea0a377c98
jesusdmora95 <jesusdmora@unicesar.edu.co>
2022-05-28 02:43:44 -0500

    Guarda pesos y umbrales no probados

--- IAMatricial/Form1.Designer.cs
+++ IAMatricial/Form1.Designer.cs
@@ -31,9 +31,12 @@ namespace IAMatricial
         {
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.tabPage = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.comboBoxRegla = new System.Windows.Forms.ComboBox();
             this.comboBoxFuncion = new System.Windows.Forms.ComboBox();
@@ -42,12 +45,22 @@ namespace IAMatricial
             this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.tabPage.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            this.groupBox3.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -63,20 +76,11 @@ namespace IAMatricial
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(160, 19);
+            this.label1.Location = new System.Drawing.Point(208, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(707, 416);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Tipo de red";
             // 
             // tabPage
             // 
@@ -90,6 +94,8 @@ namespace IAMatricial
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.groupBox5);
             this.tabPage1.Controls.Add(this.groupBox3);
             this.tabPage1.Controls.Add(this.groupBox2);
             this.tabPage1.Controls.Add(this.groupBox1);
@@ -101,13 +107,56 @@ namespace IAMatricial
             this.tabPage1.Text = "Unicapa";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.groupBox4);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(6, 6);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(335, 135);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Banco de datos";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Borrar datos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Location = new System.Drawing.Point(6, 51);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(323, 78);
+            this.groupBox4.TabIndex = 9;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Parametros de entrada";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.comboBoxRegla);
             this.groupBox2.Controls.Add(this.comboBoxFuncion);
             this.groupBox2.Location = new System.Drawing.Point(514, 6);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(159, 100);
+            this.groupBox2.Size = new System.Drawing.Size(159, 135);
             this.groupBox2.TabIndex = 14;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Datos de configuracion";
@@ -115,7 +164,7 @@ namespace IAMatricial
             // comboBoxRegla
             // 
             this.comboBoxRegla.FormattingEnabled = true;
-            this.comboBoxRegla.Location = new System.Drawing.Point(6, 70);
+            this.comboBoxRegla.Location = new System.Drawing.Point(6, 64);
             this.comboBoxRegla.Name = "comboBoxRegla";
             this.comboBoxRegla.Size = new System.Drawing.Size(139, 21);
             this.comboBoxRegla.TabIndex = 1;
@@ -137,9 +186,9 @@ namespace IAMatricial
             this.groupBox1.Controls.Add(this.checkBox1);
             this.groupBox1.Controls.Add(this.checkBox3);
             this.groupBox1.Controls.Add(this.checkBox2);
-            this.groupBox1.Location = new System.Drawing.Point(390, 6);
+            this.groupBox1.Location = new System.Drawing.Point(347, 6);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(118, 100);
+            this.groupBox1.Size = new System.Drawing.Size(161, 135);
             this.groupBox1.TabIndex = 13;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Tipo de red";
@@ -187,35 +236,108 @@ namespace IAMatricial
             this.tabPage2.Text = "Multicapa";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // groupBox3
+            // label2
             // 
-            this.groupBox3.Controls.Add(this.button1);
-            this.groupBox3.Controls.Add(this.label1);
-            this.groupBox3.Location = new System.Drawing.Point(6, 6);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(335, 100);
-            this.groupBox3.TabIndex = 15;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Datos del archivo";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Numero iteraciones";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Rata de aprendizaje";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Error maximo permitido";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(163, 72);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.textBox3);
+            this.groupBox5.Controls.Add(this.textBox2);
+            this.groupBox5.Controls.Add(this.textBox1);
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Controls.Add(this.label2);
+            this.groupBox5.Location = new System.Drawing.Point(6, 147);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(335, 135);
+            this.groupBox5.TabIndex = 16;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Parametros de entrenamiento";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 105);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Generar pesos y umbrales";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(350, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "label6";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 413);
             this.Controls.Add(this.tabPage);
-            this.Controls.Add(this.label2);
             this.Name = "Form1";
             this.Text = "IA";
             this.tabPage.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -223,7 +345,6 @@ namespace IAMatricial
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TabControl tabPage;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
@@ -235,6 +356,18 @@ namespace IAMatricial
         private System.Windows.Forms.ComboBox comboBoxRegla;
         private System.Windows.Forms.ComboBox comboBoxFuncion;
         private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- IAMatricial/Form1.cs
+++ IAMatricial/Form1.cs
@@ -13,30 +13,75 @@ namespace IAMatricial
 {
     public partial class Form1 : Form
     {
+        
         string ruta;
-        int m1 = 0, m2 = -2, entrada = -1, salida = -1, n = 0, ban=0;
+        int m1 = 0, m2 = -2, entrada = -1, salida = -1, n = 0, ban=0, archivo=0;
+        string e = "-----";
         public Form1()
         {
             InitializeComponent();
-            label1.Text = "";
-            label2.Text = "";
-            groupBox2.Enabled = false;
+            LimpiarDatos();
+            BotonCargar();
+            groupBox5.Enabled = false;
+            label6.Text ="";
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        // codigos de interfaz
+
+        private void LimpiarDatos() {
+            label1.Text = "";
+            label1.Text = "N/A x N/A \nN/A \nN/A \nN/A";
+            label3.Text = "";
+            label3.Text = "Tamaño de la matriz de datos : -" + e + e + e + "\nEntrada (E) : "
+                + e + e + e + e + e + e + e + e + e + "\nSalida (S) : ---" + e + e + e + e + e + e + e + e + e + "\nPatrones (P) : ---" + e + e + e + e + e + e + e + e;
+            ruta="";
+            m1 = 0;
+            m2 = -2;
+            entrada = -1;
+            salida = -1; 
+            n = 0; 
+            ban = 0;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            groupBox1.Enabled = false;
+            LimpiarListas();
+            groupBox5.Enabled = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            label6.Text = "";
+        }
+
+        private void LimpiarListas()
         {
-            OpenFileDialog openFile = new OpenFileDialog();
-            if (openFile.ShowDialog() == DialogResult.OK)
+            comboBoxFuncion.Items.Clear();
+            comboBoxRegla.Items.Clear();
+            comboBoxFuncion.Text = "Funcion De Activacion";
+            comboBoxRegla.Text = "Regla De Entramiento";
+            groupBox2.Enabled = false;
+            checkBox1.Enabled = true;
+            checkBox2.Enabled = true;
+            checkBox3.Enabled = true;
+            ban = 0;
+        }
+
+        private void BotonCargar()
+        {
+            if (archivo==0)
             {
-                ruta = openFile.FileName;
-                label1.Text=Leer(ruta);
+                button1.Enabled = true;
+                button2.Enabled = false;
+                archivo = 1;
             }
             else
             {
-                MessageBox.Show("No Se Pudo Cargar El Archivo");
+                button1.Enabled = false;
+                button2.Enabled = true;
+                archivo = 0;
             }
-            //LenarMatriz();
         }
+
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if (ban == 0)
@@ -118,17 +163,49 @@ namespace IAMatricial
 
         private void comboBoxFuncion_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            if (!comboBoxRegla.Text.Equals("Regla De Entramiento")&&!comboBoxFuncion.Text.Equals("Funcion De Activacion"))
+            {
+                groupBox5.Enabled = true;
+            }
         }
 
         private void comboBoxRegla_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            if (!comboBoxRegla.Text.Equals("Regla De Entramiento") && !comboBoxFuncion.Text.Equals("Funcion De Activacion"))
+            {
+                groupBox5.Enabled = true;
+            }
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LimpiarDatos();
+            BotonCargar();
+        }
+
+        // fin codigos de interfaz
+
+        //codigos de parametrizacion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                ruta = openFile.FileName;
+                label1.Text = Leer(ruta);
+                BotonCargar();
+                groupBox1.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("No Se Pudo Cargar El Archivo");
+            }
+            
+        }
+        
         private string Leer(string fileName)
         {
-            string e = "-----";
             FileStream file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
             StreamReader reader = new StreamReader(file);
             string linea;
@@ -136,12 +213,12 @@ namespace IAMatricial
             {
                 while (ParaCiclo(linea, n))
                 {
-                    if(SaberSiContunuaEntrada(linea, n))
+                    if(SaberSiContinuaEntrada(linea, n))
                     {
                         entrada++;
                         m2++;
                     }
-                    if(SaberSiContunuaSalida(linea, n))
+                    if(SaberSiContinuaSalida(linea, n))
                     {
                         salida++;
                         m2++;
@@ -150,8 +227,7 @@ namespace IAMatricial
                 }
                 m1++;
             }
-            string respuesta = "Tamaño de la matriz : -" + e +" "+ m1+" x "+m2+ "\nEntrada : " 
-                +e+e+e+e+e+ " "+entrada + "\nSalida : ---" +e+e+e+e+e+ " "+salida+ "\nPatrones : ---"+e+e+e+e+ " "+ m2;
+            string respuesta = m1+" x "+m2+ "\n"+entrada + "\n"+salida+ "\n"+ m2;
             reader.Close();
             file.Close();
             return respuesta;
@@ -159,7 +235,7 @@ namespace IAMatricial
 
         private Boolean ParaCiclo(string linea, int n)
         {
-            if (SaberSiContunuaEntrada(linea, n) || SaberSiContunuaSalida(linea, n))
+            if (SaberSiContinuaEntrada(linea, n) || SaberSiContinuaSalida(linea, n))
             {
                 return true;
             }
@@ -169,7 +245,7 @@ namespace IAMatricial
             }
         }
 
-        private Boolean SaberSiContunuaEntrada(string linea, int n)
+        private Boolean SaberSiContinuaEntrada(string linea, int n)
         {
             try
             {
@@ -182,7 +258,7 @@ namespace IAMatricial
             }
         }
 
-        private Boolean SaberSiContunuaSalida(string linea, int n)
+        private Boolean SaberSiContinuaSalida(string linea, int n)
         {
             try
             {
@@ -195,19 +271,59 @@ namespace IAMatricial
             }
         }
 
-        /*private void LenarMatriz()
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LenarMatriz();
+        }
+
+        private void LenarMatriz()
         {
             Random num = new Random();
-            double[,] Matriz = new double [m1++,m2++];
-            for(int i = 0;i <= m1; i++)
+            double recupera;
+            double[,] Matriz = new double [m2, m1];
+            double[] umbrales = new double[m2];
+            for (int i = 0;i < m2; i++)
             {
-                for (int j = 0; j <= m2; j++)
+                for (int j = 0; j < m1; j++)
                 {
-                    //Matriz[i,j] = num.Next(0,2);
+                    recupera = num.Next(-10, 11);
+                    Matriz[i, j] = (recupera) / 10;
                 }
+                recupera = num.Next(-10, 11);
+                umbrales[i]= (recupera) / 10;
             }
-            string mensaje = "" + Matriz[0, 0] + "  " +num.NextDouble()*(1+(-1))+(-1) ;
-            label2.Text = mensaje;
-        }*/
+            Guardar(Matriz,umbrales);
+        }
+
+        public void Guardar(double[,] matriz, double[] umbral)
+        {
+            string FileName = ruta.Split('.')[0] + "_Pesos_y_umbrales.txt", linea = "";
+            try
+            {
+                FileStream file = new FileStream(FileName, FileMode.Append);
+                StreamWriter writer = new StreamWriter(file);
+                for (int i = 0; i < entrada; i++)
+                {
+                    for (int j = 0; j < salida; j++)
+                    {
+                        linea = linea + matriz[j, i] + ";";
+                    }
+                    writer.WriteLine($"{linea}");
+                    linea = "";
+                }
+                for (int k = 0; k < salida; k++)
+                {
+                    linea = linea + umbral[k] + ";";
+                }
+                writer.WriteLine($"{linea}");
+                writer.Close();
+                file.Close();
+                label6.Text = "Se guardo en la rura \n" + ruta.Split('.')[0] + "_Pesos_y_umbrales.txt";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("No Se Pudo Cargar El Archivo" + e.Message);
+            }
+        }
     }
 }
